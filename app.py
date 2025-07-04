3b4438c3785132b44d4cf0bc5337ba5931a0cc4c
Manohar_vvs <manoharvvs2004@gmail.com>
2025-07-04 14:12:14 +0530

    simple files 0.0.0.1

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
 
 
 
